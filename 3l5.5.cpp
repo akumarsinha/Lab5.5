69ea76dc3336ab10cf23d896f3edf702d1bb18a4
akumarsinha <42178849+akumarsinha@users.noreply.github.com>
2018-09-08 16:04:54 +0530

    Add files via upload

---
+++ 3l5.5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter size of squar : ";
+	cin>>a;
+	for(int i=0;i<a;i++){
+		for(int j=0;j<a;j++){
+			if (j!=0 && j!=a-1 && j!=i && j!=a-1-i  && i!=0 && i!=a-1)
+			cout<<" ";
+			
+			else
+			cout<<"*";
+		}
+		cout<<endl;
+		
+	}
+	return 0;
+}
---
+++ 7l5.5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter size of triangle : ";
+	cin>>a;
+	for(int i=0;i<a;i++){
+		for(int j=0;j<i+1;j++)
+
+		if (j!=0 && j!=i && i!=0 && i!=a-1)
+			cout<<" ";
+			
+			else
+			cout<<"*";
+		cout<<endl;
+	}
+	return 0;
+}
